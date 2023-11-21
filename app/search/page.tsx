676ee59942c1ffb68bf1328160e2315e7f4c8dc9
aLearningLad <mocmanca@gmail.com>
2023-11-21 09:36:35 +0200

    utils removed

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,7 +2,6 @@
 
 import { BsSearch } from "react-icons/bs";
 import { SearchCoinTab } from "../components";
-import { AiFillStar } from "react-icons/ai";
 import { useState } from "react";
 import Link from "next/link";
 import { SearchResultType } from "@/myTypes";
